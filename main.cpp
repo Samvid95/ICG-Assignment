31d1e496f589f67a5cc5f694b808d42419079980
Samvid Jhaveri <Samvid Jhaveri>
2016-09-17 22:20:48 +0530

    The color shading is now working properly

--- main.cpp
+++ main.cpp
@@ -6,18 +6,25 @@ GLint program;
 GLuint vertPostionVBO;
 GLuint positionAttribute;
 
+GLuint vertColorVBO;
+GLuint colorAttribute;
+
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glUseProgram(program);
 	glBindBuffer(GL_ARRAY_BUFFER, vertPostionVBO);
-
 	glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(positionAttribute);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertColorVBO);
+	glVertexAttribPointer(colorAttribute, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(colorAttribute);
 
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 
 	glDisableVertexAttribArray(positionAttribute);
+	glDisableVertexAttribArray(colorAttribute);
 
 	glutSwapBuffers();
 }
@@ -29,6 +36,7 @@ void init() {
 
 	glUseProgram(program);
 	positionAttribute = glGetAttribLocation(program, "position");
+	colorAttribute = glGetAttribLocation(program, "color");
 
 	glGenBuffers(1, &vertPostionVBO);
 	glBindBuffer(GL_ARRAY_BUFFER, vertPostionVBO);
@@ -44,6 +52,18 @@ void init() {
 	};
 
 	glBufferData(GL_ARRAY_BUFFER, 12* sizeof(GLfloat), sqVert, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &vertColorVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, vertColorVBO);
+	GLfloat sqColors[24] = {
+		1.0f, 0.7f, 0.3f, 1.0f,
+		0.7f, 0.3f, 1.0f, 1.0f,
+		0.3f, 1.0f, 0.7f, 1.0f,
+		0.5f, 0.9f, 0.2f, 1.0f,
+		0.9f, 0.2f, 0.5f, 1.0f,
+		0.2f, 0.5f, 0.9f, 1.0f
+	};
+	glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(GLfloat), sqColors, GL_STATIC_DRAW);
 
 }
 
