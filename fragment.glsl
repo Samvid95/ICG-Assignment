dd121fd4e67797fd2f262a109d695195bc25a886
Samvid95 <samvid95@gmail.com>
2016-11-15 21:32:08 +0530

    Monk with texture but not good!!

--- fragment.glsl
+++ fragment.glsl
@@ -1,9 +1,12 @@
 varying vec4 varyingNormal;
+varying vec2 varyingTexCoord;
 
+uniform sampler2D diffuseTexture;
 uniform vec3 uColor;
 
 void main() {
 	float diffuse = max(0.0, dot(varyingNormal, vec4(-0.5773, 0.5773, 0.5773, 0.0)));
 	vec3 intensity = uColor * diffuse;
-	gl_FragColor = vec4(intensity.xyz, 1.0);
+	//gl_FragColor = vec4(varyingTexCoord.x,0.0,varyingTexCoord.y, 1.0);
+	gl_FragColor = texture2D(diffuseTexture,varyingTexCoord);
 }
--- vertex.glsl
+++ vertex.glsl
@@ -1,13 +1,16 @@
 attribute vec4 position;
 attribute vec4 normal;
+attribute vec2 texCoord;
 
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 uniform mat4 normalMatrix;
 
 varying vec4 varyingNormal;
+varying vec2 varyingTexCoord;
 
 void main() {
+	varyingTexCoord = texCoord;
 	varyingNormal = normalMatrix * normal;
 	gl_Position = projectionMatrix * modelViewMatrix * position;
 }
