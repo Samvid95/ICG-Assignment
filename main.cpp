8f5d400c9d77dc82a3d11a5ef222c30d02b86fab
Samvid95 <samvid95@gmail.com>
2016-11-30 23:44:12 +0530

    Only sphere genereated inside the function!

--- main.cpp
+++ main.cpp
@@ -27,10 +27,10 @@ GLuint colorAttribute;
 
 GLuint modelViewMatrixUniformLocation;
 GLuint projectionMatrixUniformLocation;
+GLuint normalMatrixUniformLocation;
 GLuint positionUniform;
 
 GLuint normalAttribute;
-GLuint normalMatrixUniformLocation;
 GLuint uColorLocation;
 
 GLuint vertexBO1, vertexBO2, vertexBO3, vertexBO4;
@@ -38,6 +38,16 @@ GLuint indexBO1, indexBO2, indexBO3, indexBO4;
 
 GLuint diffuseTexture;
 GLuint diffuseTextureUniformLocation;
+
+//BulletPhysics Initialization
+btBroadphaseInterface *broadphase;
+btDefaultCollisionConfiguration *collisionConfiguration;
+btCollisionDispatcher *dispatcher;
+btSequentialImpulseConstraintSolver *solver;
+btDiscreteDynamicsWorld *dynamicsWorld;
+
+//RigidBody
+btRigidBody *fallRigidBody;
 
 typedef struct Entity Entity;
 
@@ -189,19 +199,19 @@ void display(void) {
 	
 	//EyeMatrix
 	Matrix4 eyeMatrix;
-	eyeMatrix = eyeMatrix.makeTranslation(Cvec3(0.0, 0.0, 35.0));
+	eyeMatrix = eyeMatrix.makeTranslation(Cvec3(0.0, 3.0, 25.0));
 
 	//Projection Matrix
 	Matrix4 projectionMatrix;
 	projectionMatrix = projectionMatrix.makeProjection(45.0, 1.0, -0.1, -100.0);
-	
+	/**
 	Entity Object3D;
 	Object3D.t = Cvec3(0.0, -3.0, 0.0);
 	Object3D.r = Cvec3(0.0, 0.0, 0.0);
 	Object3D.s = Cvec3(1.0, 1.0, 1.0);
 	Object3D.parent = NULL;
 	Object3D.Draw(eyeMatrix, projectionMatrix, modelViewMatrixUniformLocation, projectionMatrixUniformLocation, normalMatrixUniformLocation, "Object3D");
-
+	*/
 	/*
 	Entity matrixA;
 	matrixA.t = Cvec3(0.0, 0.0, 0.0);
@@ -221,6 +231,22 @@ void display(void) {
 	objectC.Draw(eyeMatrix, projectionMatrix, modelViewMatrixUniformLocation, projectionMatrixUniformLocation, normalMatrixUniformLocation, "Sphere");
 	*/
 
+	Entity plane;
+	plane.t = Cvec3(0.0, 0.0, 0.0);
+	plane.r = Cvec3(0.0, 0.0, 0.0);
+	plane.s = Cvec3(1.0, 1.0, 1.0);
+	plane.parent = NULL;
+	plane.Draw(eyeMatrix, projectionMatrix, modelViewMatrixUniformLocation, projectionMatrixUniformLocation, normalMatrixUniformLocation, "Plane");
+
+	Entity sphere;
+	sphere.t = Cvec3(0.0, 5.0, 0.0);
+	sphere.r = Cvec3(0.0, 0.0, 0.0);
+	sphere.s = Cvec3(1.0, 1.0, 1.0);
+	sphere.parent = NULL;
+	sphere.Draw(eyeMatrix, projectionMatrix, modelViewMatrixUniformLocation, projectionMatrixUniformLocation, normalMatrixUniformLocation, "Sphere");
+
+
+
 	glDisableVertexAttribArray(positionAttribute);
 	glDisableVertexAttribArray(colorAttribute);
 	glDisableVertexAttribArray(normalAttribute);
@@ -230,11 +256,32 @@ void display(void) {
 
 void SphereGenerator() {
 	int ibLen, vbLen;
-	getSphereVbIbLen(8, 8, vbLen, ibLen);
+	getSphereVbIbLen(16, 16, vbLen, ibLen);
 	vector<VertexPN> vtx(vbLen);
 	vector<unsigned short> idx(ibLen);
-	makeSphere(2, 8, 8, vtx.begin(), idx.begin());
+	makeSphere(1, 16, 16, vtx.begin(), idx.begin());
 
+	btCollisionShape *fallShape = new btSphereShape(1);
+
+	btDefaultMotionState *fallMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0.0, 5.0, 0.0)));
+	btScalar mass = 1;
+	btVector3 fallInertia(0, 0, 0);
+	fallShape->calculateLocalInertia(mass, fallInertia);
+
+	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
+	fallRigidBody = new btRigidBody(fallRigidBodyCI);
+	dynamicsWorld->addRigidBody(fallRigidBody);
+	/*
+	for (int i = 0; i < 300; i++) {
+
+		dynamicsWorld->stepSimulation(1 / 60.f, 10);
+
+		btTransform trans;
+		fallRigidBody->getMotionState()->getWorldTransform(trans);
+
+		std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
+	}
+	*/
 	// fill our arrays
 	glGenBuffers(1, &vertexBO1);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBO1);
@@ -274,6 +321,7 @@ void CubeGenerator() {
 	glGenBuffers(1, &indexBO3);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBO3);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned short) * idx.size(), idx.data(), GL_STATIC_DRAW);
+
 }
 
 void init() {
@@ -285,7 +333,7 @@ void init() {
 	glReadBuffer(GL_BACK);
 
 
-	glClearColor(1, 1, 1, 0.0);
+	glClearColor(0.2, 0.2, 0.2, 0.0);
 	program = glCreateProgram();
 	readAndCompileShader(program, "vertex.glsl", "fragment.glsl");
 
@@ -402,56 +450,24 @@ void idle(void) {
 	glutPostRedisplay();
 }
 
+void bulletPhysicsInit() {
+	broadphase = new btDbvtBroadphase();
+
+	collisionConfiguration = new btDefaultCollisionConfiguration();
+	dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+	solver = new btSequentialImpulseConstraintSolver;
+
+	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+	dynamicsWorld->setGravity(btVector3(0, -10, 0));
+}
+
 int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
 	glutCreateWindow("CS - 6533");
-	
-	btBroadphaseInterface* broadphase = new btDbvtBroadphase();
-	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
-	btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
-	
-	btGImpactCollisionAlgorithm::registerAlgorithm(dispatcher);
-	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
-
-	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
-
-	dynamicsWorld->setGravity(btVector3(0, -10, 0));
-
-	btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
-	btCollisionShape* fallShape = new btSphereShape(1);
-
-	btDefaultMotionState* groundMotionState =
-		new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
-
-	btRigidBody::btRigidBodyConstructionInfo
-		groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
-	btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
-
-	dynamicsWorld->addRigidBody(groundRigidBody);
-
-	btDefaultMotionState* fallMotionState =
-		new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
-
-	btScalar mass = 1;
-	btVector3 fallInertia(0, 0, 0);
-	fallShape->calculateLocalInertia(mass, fallInertia);
-
-	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
-	btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
-	dynamicsWorld->addRigidBody(fallRigidBody);
-
-	for (int i = 0; i < 300; i++) {
-
-		dynamicsWorld->stepSimulation(1 / 60.f, 10);
-
-		btTransform trans;
-		fallRigidBody->getMotionState()->getWorldTransform(trans);
-
-		std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
-	}
-
+	bulletPhysicsInit();
 	glewInit();
 
 	glutDisplayFunc(display);
@@ -463,14 +479,11 @@ int main(int argc, char **argv) {
 	init();
 	glutMainLoop();
 
-	
-	
 	delete dynamicsWorld;
 	delete solver;
 	delete dispatcher;
 	delete collisionConfiguration;
 	delete broadphase;
-	
 
 	return 0;
 }
